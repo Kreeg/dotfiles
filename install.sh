258ac1561e6b3be29e4be0415608afbd5824dd4a
Nelyubin Andrey <nelyubin_a_a@sunlight.net>
2021-09-07 16:56:20 +0300

    install nvm adding .gitignore

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/inst
 brew install tmux
 brew install nvim
 brew install zsh
+brew install nvm
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew install fzf
 curl -OL https://github.com/arl/gitmux/releases/download/v0.7.4/gitmux_0.7.4_macOS_amd64.tar.gz
