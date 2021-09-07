470fe72a73da5acab9a3b311172cdc56d137427f
Andrew <1666622+Kreeg@users.noreply.github.com>
2021-09-07 14:06:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ brew install fzf
 curl -OL https://github.com/arl/gitmux/releases/download/v0.7.4/gitmux_0.7.4_macOS_amd64.tar.gz
 tar -xzf gitmux_0.7.4_macOS_amd64.tar.gz
 chmod +x gitmux
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
