42fda5e61d2f74fea4d01525148b8c2ade281cc3
Andrew <1666622+Kreeg@users.noreply.github.com>
2021-09-07 14:09:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ tar -xzf gitmux_0.7.4_macOS_amd64.tar.gz
 chmod +x gitmux
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+mkdir $HOME/alacritty
